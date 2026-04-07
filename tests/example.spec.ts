b6807eee2175ac8e7b5b1532e4ad62a28d3246c9
pandeyji17493 <pandeyji17493@gmail.com>
2026-04-07 21:25:51 +0530

    Fix test function parameter from 'go' to 'page'

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -7,7 +7,7 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('get started link', async ({ go }) => {
+test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
